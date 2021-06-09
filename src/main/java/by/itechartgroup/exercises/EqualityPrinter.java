1939c1f19439bc12b63233a98190ed503569fdce
Elovard <persall2013@gmail.com>
2021-06-09 14:31:24 +0300

    .

---
+++ src/main/java/by/itechartgroup/exercises/EqualityPrinter.java
@@ -0,0 +1,19 @@
+package by.itechartgroup.exercises;
+
+public class EqualityPrinter {
+
+    public static void main(String[] args) {
+        printEqual(1,2,3);
+    }
+
+    public static void printEqual(int param1, int param2, int param3){
+        if(param1 < 0 || param2 < 0 || param3 < 0)
+            System.out.println("Invalid value");
+        if(param1 == param2 && param2 == param3){
+            System.out.println("All numbers are equal");
+        } else if(param1 == param2 || param1 == param3 || param2 == param3){
+            System.out.println("Neither all are equal or different");
+        } else
+            System.out.println("All numbers are different");
+    }
+}
---
+++ src/main/java/by/itechartgroup/exercises/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,16 @@
+package by.itechartgroup.exercises;
+
+public class MinutesToYearsAndDaysCalculator {
+
+    public static void main(String[] args) {
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes){
+        if(minutes <= 0)
+            System.out.println("Invalid value");
+        long years = minutes / 525600;
+        long days = (minutes % 525600) / 1400;
+        System.out.println(years + " years, " + days + " d");
+    }
+}
