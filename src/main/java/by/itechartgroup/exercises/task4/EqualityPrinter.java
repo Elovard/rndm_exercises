336ced9645702f4babd3708abfcc6168f46f13f7
Elovard <persall2013@gmail.com>
2021-06-10 14:06:00 +0300

    do changes in tasks 2,3,4,7 and 13

--- src/main/java/by/itechartgroup/exercises/task4/EqualityPrinter.java
+++ src/main/java/by/itechartgroup/exercises/task4/EqualityPrinter.java
@@ -2,14 +2,17 @@ package by.itechartgroup.exercises.task4;
 
 public class EqualityPrinter {
 
-    public static void printEqual(int param1, int param2, int param3){
-        if(param1 < 0 || param2 < 0 || param3 < 0)
-            System.out.println("Invalid value");
-        if(param1 == param2 && param2 == param3){
-            System.out.println("All numbers are equal");
-        } else if(param1 == param2 || param1 == param3 || param2 == param3){
-            System.out.println("Neither all are equal or different");
-        } else
-            System.out.println("All numbers are different");
+    // TODO: 6/10/21 Return string instead of sout-ing
+    public static String printEqual(int param1, int param2, int param3) {
+        if (param1 < 0 || param2 < 0 || param3 < 0) {
+            return "Invalid value";
+        }
+        if (param1 == param2 && param2 == param3) {
+            return "All numbers are equal";
+        }
+        if (param1 == param2 || param1 == param3 || param2 == param3) {
+            return "Neither all are equal or different";
+        }
+        return "All numbers are different";
     }
 }
--- src/main/java/by/itechartgroup/exercises/task7/MegaBytesConverter.java
+++ src/main/java/by/itechartgroup/exercises/task7/MegaBytesConverter.java
@@ -2,13 +2,13 @@ package by.itechartgroup.exercises.task7;
 
 public class MegaBytesConverter {
 
-    public static void printMegaBytesAndKiloBytes(int kiloBytes){
-        if(kiloBytes < 0){
-            System.out.println("Invalid value");
-        } else {
-            int megaBytes = kiloBytes / 1000;
-            int remainder = kiloBytes % 1000;
-            System.out.println(kiloBytes + " KB is " + megaBytes + " MB and " + remainder + " KB");
+    public static String printMegaBytesAndKiloBytes(int kiloBytes) throws Exception {
+        if (kiloBytes < 0) {
+            throw new Exception("Invalid value");
+            // TODO: 6/10/21 Throw custom exception
         }
+        int megaBytes = kiloBytes / 1000;
+        int remainder = kiloBytes % 1000;
+        return (kiloBytes + " KB is " + megaBytes + " MB and " + remainder + " KB");
     }
 }
