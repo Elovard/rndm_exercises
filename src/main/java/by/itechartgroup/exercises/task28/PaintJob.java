e8c83ce0ece092fda027b64b3b2e0d7537953a2c
Elovard <persall2013@gmail.com>
2021-06-14 16:09:58 +0300

    do several changes in task 28

--- src/main/java/by/itechartgroup/exercises/task28/PaintJob.java
+++ src/main/java/by/itechartgroup/exercises/task28/PaintJob.java
@@ -4,7 +4,7 @@ public class PaintJob {
 
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
         if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets <= 0) {
-            return -1;
+            throw new IllegalArgumentException("zero argument");
         }
         double totalArea = width * height;
         double totalBuckets = totalArea / areaPerBucket;
@@ -14,7 +14,7 @@ public class PaintJob {
 
     public static int getBucketCount(double width, double height, double areaPerBucket) {
         if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
-            return -1;
+            throw new IllegalArgumentException("zero argument");
         }
         double totalArea = width * height;
         double totalBuckets = totalArea / areaPerBucket;
@@ -23,7 +23,7 @@ public class PaintJob {
 
     public static int getBucketCount(double area, double areaPerBucket) {
         if (area <= 0 || areaPerBucket <= 0) {
-            return -1;
+            throw new IllegalArgumentException("zero argument");
         }
         double result = area / areaPerBucket;
         return (int) Math.ceil(result);
--- src/test/java/by/itechartgroup/exercises/task28/PaintJobTest.java
+++ src/test/java/by/itechartgroup/exercises/task28/PaintJobTest.java
@@ -1,41 +1,26 @@
 package by.itechartgroup.exercises.task28;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PaintJobTest {
 
-    @ParameterizedTest // TODO: 6/14/21 ASK: about these tests
-    @MethodSource
-    void WhenOneOfParametersIsNegativeOrZeroInFourParameters_ThenResultShouldBeFalse(double width, double height,
-                                                                                     double areaPerBucket, int extraBuckets) {
-        int actualResult = PaintJob.getBucketCount(width, height, areaPerBucket, extraBuckets);
-        assertFalse(actualResult > 0);
-    }
-
     @ParameterizedTest
     @MethodSource
-    void WhenOneOfParametersIsNegativeOrZeroInThreeParameters_ThenResultShouldBeFalse(double width, double height,
-                                                                                      double areaPerBucket) {
-        int actualResult = PaintJob.getBucketCount(width, height, areaPerBucket);
-        assertFalse(actualResult > 0);
+    void whenOneOfParametersIsNegativeOrZeroInFourParameters_ThenExceptionShouldBeThrown(double width,
+                                                                                         double height,
+                                                                                         double areaPerBucket,
+                                                                                         int extraBuckets) {
+        assertThrows(IllegalArgumentException.class, () ->
+                PaintJob.getBucketCount(width, height, areaPerBucket, extraBuckets));
     }
 
-    @ParameterizedTest
-    @MethodSource
-    void WhenOneOfParametersIsNegativeOrZeroInTwoParameters_ThenResultShouldBeFalse(double area, double areaPerBucket) {
-        int actualResult = PaintJob.getBucketCount(area, areaPerBucket);
-        assertFalse(actualResult > 0);
-    }
-
-
-    private static Stream<Arguments> WhenOneOfParametersIsNegativeOrZeroInFourParameters_ThenResultShouldBeFalse() {
+    private static Stream<Arguments> whenOneOfParametersIsNegativeOrZeroInFourParameters_ThenExceptionShouldBeThrown() {
         return Stream.of(
                 Arguments.of(0, 0, 0, 0),
                 Arguments.of(1, 2, 3, -1),
@@ -44,7 +29,15 @@ class PaintJobTest {
         );
     }
 
-    private static Stream<Arguments> WhenOneOfParametersIsNegativeOrZeroInThreeParameters_ThenResultShouldBeFalse() {
+    @ParameterizedTest
+    @MethodSource
+    void whenOneOfParametersIsNegativeOrZeroInThreeParameters_ThenExceptionShouldBeThrown(double width,
+                                                                                          double height,
+                                                                                          double areaPerBucket) {
+        assertThrows(IllegalArgumentException.class, () -> PaintJob.getBucketCount(width, height, areaPerBucket));
+    }
+
+    private static Stream<Arguments> whenOneOfParametersIsNegativeOrZeroInThreeParameters_ThenExceptionShouldBeThrown() {
         return Stream.of(
                 Arguments.of(1, -1, -10),
                 Arguments.of(-2, -3, -4),
@@ -52,7 +45,13 @@ class PaintJobTest {
         );
     }
 
-    private static Stream<Arguments> WhenOneOfParametersIsNegativeOrZeroInTwoParameters_ThenResultShouldBeFalse() {
+    @ParameterizedTest
+    @MethodSource
+    void whenOneOfParametersIsNegativeOrZeroInTwoParameters_ThenExceptionShouldBeThrown(double area, double areaPerBucket) {
+        assertThrows(IllegalArgumentException.class, () -> PaintJob.getBucketCount(area, areaPerBucket));
+    }
+
+    private static Stream<Arguments> whenOneOfParametersIsNegativeOrZeroInTwoParameters_ThenExceptionShouldBeThrown() {
         return Stream.of(
                 Arguments.of(-1, 2),
                 Arguments.of(5, -10),
