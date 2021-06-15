f74ab4312f6cc876bf9eff4b827a62dde3544d33
Elovard <persall2013@gmail.com>
2021-06-15 17:03:09 +0300

    simplify task 10

--- src/main/java/by/itechartgroup/exercises/task10/NumberOfDaysInMonth.java
+++ src/main/java/by/itechartgroup/exercises/task10/NumberOfDaysInMonth.java
@@ -3,31 +3,25 @@ package by.itechartgroup.exercises.task10;
 public class NumberOfDaysInMonth {
 
     public static boolean isLeapYear(int year) {
-        return ((year > 0 && year <= 9999) && ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0)));
+        boolean isValidYear = year > 0 && year <= 9999;
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+        return (isValidYear && isLeapYear);
     }
 
     public static int getDaysInMonth(final int month, final int year) {
-        // TODO: 6/10/21 remove first arg
+
         int[] dayInMonthCalendar = {31, 28, 31, 30, 31, 30, 31, 31, 30, 30, 30, 31};
 
-        // TODO: 6/10/21 fix naming
-        int daysInMonth = 0;
         if ((month <= 0 || month > 12 || (year <= 0 || year > 9999))) {
             return -1;
         }
 
         boolean leapYear = isLeapYear(year);
 
-        // TODO: 6/10/21 refactor (simplify)
+
         if (!leapYear) {
-            daysInMonth = dayInMonthCalendar[month];
-        } else {
             dayInMonthCalendar[1]++;
-            daysInMonth = dayInMonthCalendar[month];
-            if (month == 1) {
-                daysInMonth = dayInMonthCalendar[month];
-            }
         }
-        return daysInMonth;
+        return dayInMonthCalendar[month];
     }
 }
