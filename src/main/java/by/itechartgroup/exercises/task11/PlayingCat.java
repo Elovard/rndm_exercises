554e817fc44a281f20fba904a47762d3ef894b84
Elovard <persall2013@gmail.com>
2021-06-15 17:06:13 +0300

    simplify task 11

--- src/main/java/by/itechartgroup/exercises/task11/PlayingCat.java
+++ src/main/java/by/itechartgroup/exercises/task11/PlayingCat.java
@@ -2,12 +2,13 @@ package by.itechartgroup.exercises.task11;
 
 public class PlayingCat {
 
-    public static boolean isCatPlaying(boolean summer, int temperature){
-        if(!summer && temperature >=25 && temperature <=35){
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (!summer && temperature >= 25 && temperature <= 35) {
             return true;
-        } else if (summer && temperature >=25 && temperature <=45){
+        }
+        if (summer && temperature >= 25 && temperature <= 45) {
             return true;
-        } else
-            return false;
+        }
+        return false;
     }
 }
